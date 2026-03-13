e2892c1d6b83c6d6b7453d6ab4d358efe03bffc5
Ahamed-Aneek <aneekahamed58@gmail.com>
2026-03-13 22:38:14 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
-import * as f from "lodash";
-import { currentYear } from "./date";
-import { currentMonth } from "./date";
-function App() {
+"";
+import { hotels } from "./helper";
+const food = ["parata", "rice", "kottu", "biriyani"];
+export const Hotels = function ({ hotel }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-export const date = () => {
-  const month = new Date();
-  const stringMonth = new Intl.DateTimeFormat("en-US", {
-    month: "long",
-  }).format(month);
-  console.log(stringMonth);
-  return stringMonth;
-};
-export const data = Array.from({ length: 31 }, (_, i) => i + 1);
-console.log(data);
-export const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"].map(
-  (e) => <li>{f.capitalize(e)}</li>,
-);
-const day1 = (date) => {
-  const c = new Intl.DateTimeFormat("en-US", {
-    weekday: "short",
-  }).format(date);
-  return ["mon", "tue", "wed", "thu", "fri", "sat", "sun"].indexOf(
-    c.toLowerCase(),
+    <ol className="hotels">
+      {hotels[hotel].map((e) => (
+        <li className="arrange">
+          <span>Shop:{e.Name}</span>
+          <span>Location:{e.Location}</span>
+          <img src={e.image} alt="h"></img>
+        </li>
+      ))}
+    </ol>
   );
 };
-
-export const FindCol = (num) => {
-  return day1(new Date(currentYear(), currentMonth(), num));
+export const Foods = function ({ fun }) {
+  return (
+    <header className="top">
+      <select onChange={fun}>
+        {food.map((e) => (
+          <option>{e}</option>
+        ))}
+      </select>
+    </header>
+  );
 };
--- src/helper.js
+++ src/helper.js
@@ -1,32 +1,12 @@
-"";
-import { data } from "./App";
-import { FindCol } from "./App";
-import { date } from "./App";
-import { currentYear } from "./date";
-import { currentMonth } from "./date";
-import { currentDate } from "./date";
-const isMonth = (date1) => {
-  const isTrue = new Intl.DateTimeFormat("en-US", { month: "long" }).format(
-    date1,
-  );
-  return isTrue === date();
+export const hotels = {
+  parata: [
+    { Name: "veppam thoppu", Location: "oluvil" },
+    { Name: "maruthamunai shop", Location: "maruthamunai" },
+    { Name: "Mr Chicken", Location: "sainthamaruthu" },
+    { Name: "Akkaraipattu Shop", Location: "Akp" },
+  ],
+  kottu: [
+    { Name: "Bismillah", Location: "maligaikadu", image: "bismillah.jpg" },
+    { Name: "Thaj hotel", Location: "Kalmunai" },
+  ],
 };
-export const abstract = data.map((e) => {
-  return (
-    isMonth(new Date(currentYear(), currentMonth(), e)) && (
-      <span
-        style={{
-          textAlign: "center",
-          gridColumn: `${FindCol(e) + 1}/${FindCol(e) + 2}`,
-        }}
-        className={currentDate() === e && "now"}
-      >
-        {e}
-      </span>
-    )
-  );
-});
-console.log(data);
-console.log("welcome to new branch");
-console.log("hi");
-console.log("it is in github");
